e4fb166b405fe632b1268b64592e3ffa18748f87
Artur Bartczak <artur.bartczak@proexe.pl>
2021-10-06 23:41:26 +0200

    Added summary to Bar movie service response.

--- External/Bar/Movies/MovieService.php
+++ External/Bar/Movies/MovieService.php
@@ -19,8 +19,14 @@ class MovieService
 
         return [
             'titles' => [
-                ['title' => "Star Wars: Episode IV - A New Hope"],
-                ['title' => "The Devil and Miss Jones"],
+                [
+                    'title' => "Star Wars: Episode IV - A New Hope",
+                    'summary' => "After Princess Leia, the leader of the Rebel Alliance, is held hostage by Darth Vader, Luke and Han Solo must free her and destroy the powerful weapon created by the Galactic Empire."
+                ],
+                [
+                    'title' => "The Devil and Miss Jones",
+                    'summary' => "John P Merrick, the world's richest man, is annoyed to hear workers at one of his stores are trying to form a union. Getting a menial job, he's determined to root out the troublemakers ..."
+                ],
             ]
         ];
     }
