88db885050bab071cdfddc14578d333300b27ce1
senaszel <marekstanislawmeyer@gmail.com>
2023-01-24 22:55:43 +0100

    my work

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use External\Foo\Auth\AuthWS;
+use External\Bar\Auth\LoginService;
+use External\Baz\Auth\Authenticator;
+use External\Baz\Auth\Responses\Success;
 
 class AuthController extends Controller
 {
@@ -15,6 +19,42 @@ class AuthController extends Controller
     public function login(Request $request): JsonResponse
     {
         // TODO
+        $content =  json_decode($request->getContent(), true);
+        $login = $content['login'];
+        $password = $content['password'];
+
+        $services = [
+            "FOO_" =>   [
+                "name" => new AuthWS(),
+                "method" => "authenticate",
+                "success" => null
+            ],
+            "BAR_" =>   [
+                "name" => new LoginService(),
+                "method" => "login",
+                "success" => true
+            ],
+            "BAZ_" =>   [
+                "name" => new Authenticator(),
+                "method" => "auth",
+                "success" => new Success()
+            ]
+        ];
+
+        if (array_key_exists(substr($login, 0, 4), $services)) {
+            $service = $services[substr($login, 0, 4)];
+            if (
+                $service["name"]
+                ->{$service["method"]}($login, $password)
+                == $service["success"]
+            ) {
+                return response()->json([
+                    "status" => "success",
+                    "token" => "<generated token>"
+                    // I have 20 minutes left so I`ll rather try speedrun second task instead of using JWT for the first time. Although It seems pretty straight forward. AES-CBD-256 en/de_coding.
+                ]);
+            }
+        }
 
         return response()->json([
             'status' => 'failure',
--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use External\Bar\Movies\MovieService as BarMovieService;
+use External\Baz\Movies\MovieService as BazMovieService;
+use External\Foo\Movies\MovieService as FooMovieService;
 
 class MovieController extends Controller
 {
@@ -15,7 +18,19 @@ class MovieController extends Controller
     public function getTitles(Request $request): JsonResponse
     {
         // TODO
+        $services = [
+            new BarMovieService(),
+            new BazMovieService(),
+            new FooMovieService()
+        ];
 
-        return response()->json([]);
+        $titles = [];
+        foreach ($services as $movieService) {
+            $titles[] = array_merge($titles, $movieService->getTitles());
+            // I run out of time but I would use transforms and or maps to standardize resulting collection of titles.
+            // That should be also error proofed and cached.  
+        }
+
+        return response()->json([$titles]);
     }
 }
